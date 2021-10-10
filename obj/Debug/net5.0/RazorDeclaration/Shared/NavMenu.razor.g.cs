83615425ffbc6d1b829451194c72cb7c5d394007
Yassa Taiseer <yassataiseer@gmail.com>
2021-10-09 23:41:20 -0400

    commit

--- obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -90,7 +90,7 @@ using TikTechCRM.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 28 "/Users/yassa/TikTechCRM/Shared/NavMenu.razor"
+#line 33 "/Users/yassa/TikTechCRM/Shared/NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
