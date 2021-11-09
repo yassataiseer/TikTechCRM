77679ee151e705beec7a6178b0714cd579a349ae
Yassa Taiseer <yassataiseer@gmail.com>
2021-11-09 13:20:46 -0500

    client

---
+++ ClientModel.cs
@@ -0,0 +1,11 @@
+public class ClientModel
+{
+    public string Address { get; set; }
+    public bool Deleted { get; set; }
+    public string Email { get; set; }
+    public int Id { get; set; }
+    public string Phone_Number { get; set; }
+    public string Postal_code { get; set; }
+    public string Username { get; set; }
+}
+
